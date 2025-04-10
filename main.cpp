427877da0277c53b54ccdaa10ceb90b937331f07
yankupchinskiy <yaks06@mail.ru>
2025-04-10 15:56:24 +0300

    2

--- main.cpp
+++ main.cpp
@@ -28,8 +28,33 @@ int main() {
 
     std::cout << "PNG file verified successfully\n";
 
-    png_structp image = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
+    if (!png_ptr){
+        std::cout <<"png struct creation failed";
+        return EXIT_FAILURE;
+    }
 
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+
+    if (!info_ptr)
+    {
+       png_destroy_read_struct(&png_ptr, NULL, NULL);
+       std::cout <<"png struct information failed";
+       return EXIT_FAILURE;
+    }
+
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        std::cerr << "libpng error\n";
+        png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
+        fclose(opened_file);
+        return EXIT_FAILURE;
+    }
+
+    png_init_io(png_ptr, opened_file);
+
+    png_set_sig_bytes(png_ptr,8);
+    
+    png_read_info(png_ptr, info_ptr);
 
     fclose(opened_file);
     return EXIT_SUCCESS;
